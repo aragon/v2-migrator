690a28d2858cf9ecd658781ccc8538f1911319ec
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2021-05-01 15:52:43 -0300

    subgraph: fix migration decoding

--- packages/subgraph/utils/migration.ts
+++ packages/subgraph/utils/migration.ts
@@ -19,6 +19,8 @@ const FINANCE_TRANSFER_CALLDATA_LENGTH = '000000e4'
  * [ vault address | calldata length | fn selector | token address | executor address |  amount  ]
  * [    20 bytes   |     4 bytes     |   4 bytes   |    32 bytes   |     32 bytes     | 32 bytes ]
  *
+ * Example: 0x000000016e8d25b8d912827e19bb070153e48e38ddbe5c3a00000064beabacc80000000000000000000000005bc9be34f98eb072696d63b5be5d4d2f2c03d0ad000000000000000000000000fbe66da8066711d53cf7b23f701f53f9814e8b9e0000000000000000000000000000000000000000000001b1ae4d6e2ef50000006e8d25b8d912827e19bb070153e48e38ddbe5c3a00000064beabacc8000000000000000000000000e9a083d88eed757b1d633321ce0519f432c6284d000000000000000000000000fbe66da8066711d53cf7b23f701f53f9814e8b9e00000000000000000000000000000000000000000000003635c9adc5dea000006e8d25b8d912827e19bb070153e48e38ddbe5c3a00000064beabacc80000000000000000000000000000000000000000000000000000000000000000000000000000000000000000fbe66da8066711d53cf7b23f701f53f9814e8b9e00000000000000000000000000000000000000000000000001cdda4faccd0000
+ *
  * Which means that the call script body should have a length of N * 124 bytes = N * 248 chars, or
  *
  * 2. Through finance app:
@@ -26,13 +28,15 @@ const FINANCE_TRANSFER_CALLDATA_LENGTH = '000000e4'
  * [ finance address | calldata length | fn selector | token address | executor address |  amount  | reference ]
  * [     20 bytes    |     4 bytes     |   4 bytes   |    32 bytes   |     32 bytes     | 32 bytes | 128 bytes ]
  *
+ * Example: 0x0000000173237ba75084d95ada526dcf27ccc9eb99b31747000000e4f63648460000000000000000000000005bc9be34f98eb072696d63b5be5d4d2f2c03d0ad000000000000000000000000fbe66da8066711d53cf7b23f701f53f9814e8b9e0000000000000000000000000000000000000000000001b1ae4d6e2ef5000000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000215472616e736665722066756e647320746f20676f7665726e206578656375746f720000000000000000000000000000000000000000000000000000000000000073237ba75084d95ada526dcf27ccc9eb99b31747000000e4f6364846000000000000000000000000e9a083d88eed757b1d633321ce0519f432c6284d000000000000000000000000fbe66da8066711d53cf7b23f701f53f9814e8b9e00000000000000000000000000000000000000000000003635c9adc5dea00000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000215472616e736665722066756e647320746f20676f7665726e206578656375746f7200000000000000000000000000000000000000000000000000000000000000
+ *
  * Which means that the call script body should have a length of N * 252 bytes = N * 504 chars
  *
  */
 export function decodeMigrationType(voting: VotingEntity, script: string): number {
   // Assert given script is a call script
   const scriptHeader = script.substring(2, 10)
-  if (scriptHeader !== CALL_SCRIPT_ID) return 0
+  if (scriptHeader != CALL_SCRIPT_ID) return 0
 
   const scriptBody = script.slice(10)
   const dao = DAOEntity.load(voting.dao)!
@@ -60,12 +64,12 @@ function isValidVaultMigration(dao: DAOEntity, scriptBody: string): boolean {
   let recipient = ''
 
   // Assert every call in the script body is a vault transfer to a registered govern executor
-  for (let i = 0; i < scriptBody.length / VAULT_CALL_SCRIPT_MIN_LENGTH; i++) {
-    const call = scriptBody.slice(i, i * VAULT_CALL_SCRIPT_MIN_LENGTH)
+  for (let i = 0; i < scriptBody.length; i += VAULT_CALL_SCRIPT_MIN_LENGTH) {
+    const call = scriptBody.slice(i, i + VAULT_CALL_SCRIPT_MIN_LENGTH)
 
     // Assert call target is the DAO's vault
-    const vault = `0x${call.substring(0, 40)}`
-    if (!dao.vault || dao.vault.toHexString() !== vault) return false
+    const vault = '0x' + call.substring(0, 40)
+    if (!dao.vault || dao.vault.toHexString() != vault) return false
 
     // Assert call data length matches a vault transfer
     const length = call.substring(40, 48)
@@ -84,8 +88,8 @@ function isValidVaultMigration(dao: DAOEntity, scriptBody: string): boolean {
     if (rawRecipientAddress.length != 40) return false
 
     // Assert recipient is equal to the previous decoded recipient
-    if (recipient != '' && recipient != `0x${rawRecipientAddress}`) return false
-    else recipient = `0x${rawRecipientAddress}`
+    if (recipient != '' && recipient != '0x' + rawRecipientAddress) return false
+    else recipient = '0x' + rawRecipientAddress
 
     // Assert transfer amount
     const rawTransferAmount = call.substring(184, 248)
@@ -100,12 +104,12 @@ function isValidFinanceMigration(dao: DAOEntity, scriptBody: string): boolean {
   let recipient = ''
 
   // Assert every call in the script body is a finance transfer to a registered govern executor
-  for (let i = 0; i < scriptBody.length / FINANCE_CALL_SCRIPT_MIN_LENGTH; i++) {
-    const call = scriptBody.slice(i, i * FINANCE_CALL_SCRIPT_MIN_LENGTH)
+  for (let i = 0; i < scriptBody.length; i += FINANCE_CALL_SCRIPT_MIN_LENGTH) {
+    const call = scriptBody.slice(i, i + FINANCE_CALL_SCRIPT_MIN_LENGTH)
 
     // Assert call target is the DAO's finance app
-    const finance = `0x${call.substring(0, 40)}`
-    if (!dao.finance || dao.finance.toHexString() !== finance) return false
+    const finance = "0x" + call.substring(0, 40)
+    if (!dao.finance || dao.finance.toHexString() != finance) return false
 
     // Assert call data length matches a finance transfer
     const length = call.substring(40, 48)
@@ -124,8 +128,8 @@ function isValidFinanceMigration(dao: DAOEntity, scriptBody: string): boolean {
     if (rawRecipientAddress.length != 40) return false
 
     // Assert recipient is equal to the previous decoded recipient
-    if (recipient != '' && recipient != `0x${rawRecipientAddress}`) return false
-    else recipient = `0x${rawRecipientAddress}`
+    if (recipient != '' && recipient != '0x' + rawRecipientAddress) return false
+    else recipient = '0x' + rawRecipientAddress
 
     // Assert transfer amount
     const rawTransferAmount = call.substring(184, 248)
@@ -145,12 +149,12 @@ export function decodeExecutor(script: string, migrationType: number): Address {
     // Vault migration
     const call = script.slice(10, VAULT_CALL_SCRIPT_MIN_LENGTH)
     const rawRecipientAddress = call.substring(144, 184)
-    return Address.fromString(`0x${rawRecipientAddress}`)
+    return Address.fromString('0x' + rawRecipientAddress)
   } else {
     // Finance migration
     const call = script.slice(10, FINANCE_CALL_SCRIPT_MIN_LENGTH)
     const rawRecipientAddress = call.substring(144, 184)
-    return Address.fromString(`0x${rawRecipientAddress}`)
+    return Address.fromString('0x' + rawRecipientAddress)
   }
 }
 
@@ -160,18 +164,18 @@ export function decodeTokens(script: string, migrationType: number): Address[] {
 
   if (migrationType == 1) {
     // Vault migration
-    for (let i = 0; i < scriptBody.length / VAULT_CALL_SCRIPT_MIN_LENGTH; i++) {
-      const call = scriptBody.slice(i, i * VAULT_CALL_SCRIPT_MIN_LENGTH)
+    for (let i = 0; i < scriptBody.length; i += VAULT_CALL_SCRIPT_MIN_LENGTH) {
+      const call = scriptBody.slice(i, i + VAULT_CALL_SCRIPT_MIN_LENGTH)
       const rawTokenAddress = call.substring(80, 120)
-      const token = Address.fromString(`0x${rawTokenAddress}`)
+      const token = Address.fromString('0x' + rawTokenAddress)
       tokens.push(token)
     }
   } else {
     // Finance migration
-    for (let i = 0; i < scriptBody.length / FINANCE_CALL_SCRIPT_MIN_LENGTH; i++) {
-      const call = scriptBody.slice(i, i * FINANCE_CALL_SCRIPT_MIN_LENGTH)
+    for (let i = 0; i < scriptBody.length; i += FINANCE_CALL_SCRIPT_MIN_LENGTH) {
+      const call = scriptBody.slice(i, i + FINANCE_CALL_SCRIPT_MIN_LENGTH)
       const rawTokenAddress = call.substring(80, 120)
-      const token = Address.fromString(`0x${rawTokenAddress}`)
+      const token = Address.fromString('0x' + rawTokenAddress)
       tokens.push(token)
     }
   }
@@ -185,18 +189,18 @@ export function decodeAmounts(script: string, migrationType: number): BigInt[] {
 
   if (migrationType == 1) {
     // Vault migration
-    for (let i = 0; i < scriptBody.length / VAULT_CALL_SCRIPT_MIN_LENGTH; i++) {
-      const call = scriptBody.slice(i, i * VAULT_CALL_SCRIPT_MIN_LENGTH)
+    for (let i = 0; i < scriptBody.length; i += VAULT_CALL_SCRIPT_MIN_LENGTH) {
+      const call = scriptBody.slice(i, i + VAULT_CALL_SCRIPT_MIN_LENGTH)
       const rawTransferAmount = call.substring(184, 248)
-      const balance = BigInt.fromUnsignedBytes(Bytes.fromHexString(`0x${rawTransferAmount}`) as Bytes)
+      const balance = BigInt.fromUnsignedBytes(Bytes.fromHexString('0x' + rawTransferAmount).reverse() as Bytes)
       balances.push(balance)
     }
   } else {
     // Finance migration
-    for (let i = 0; i < scriptBody.length / FINANCE_CALL_SCRIPT_MIN_LENGTH; i++) {
-      const call = scriptBody.slice(i, i * FINANCE_CALL_SCRIPT_MIN_LENGTH)
+    for (let i = 0; i < scriptBody.length; i += FINANCE_CALL_SCRIPT_MIN_LENGTH) {
+      const call = scriptBody.slice(i, i + FINANCE_CALL_SCRIPT_MIN_LENGTH)
       const rawTransferAmount = call.substring(184, 248)
-      const balance = BigInt.fromUnsignedBytes(Bytes.fromHexString(`0x${rawTransferAmount}`) as Bytes)
+      const balance = BigInt.fromUnsignedBytes(Bytes.fromHexString('0x' + rawTransferAmount).reverse() as Bytes)
       balances.push(balance)
     }
   }
@@ -205,13 +209,14 @@ export function decodeAmounts(script: string, migrationType: number): BigInt[] {
 }
 
 function occurrences(text: string, word: string): number {
-  let index = 0, n = 0;
+  let index = 0, count = 0;
 
   while (true) {
-    index = text.indexOf(word, index + 1);
-    if (index >= 0) n++;
-    else break;
+    index = text.indexOf(word, index)
+    if (index < 0) break;
+    count++;
+    index += 1;
   }
 
-  return n;
+  return count;
 }
